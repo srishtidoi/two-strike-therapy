942e29eb12011373d4b5fea5921e2290f32eb852
srishtidoi <srishpat@gmail.com>
2024-05-05 17:31:43 +0530

    high extinction prob regions heatmap scripts

---
+++ plotting/EP_parameter_space.R
@@ -0,0 +1,184 @@
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(reshape2)
+  library(tidyverse)
+  library(deSolve)
+})
+# Calculates extinction probabilities for a range of
+# delta_1 - delta_2 values (uncommented) 
+# and b - d values (commented)
+# for multiple switching points (N(tau) values)
+
+# edit this block 
+#################################################################
+
+# path relative to the working directory
+main_dir <- "optimal_doses/data_default" # for plots like Fig2A
+# main_dir <- "b-d_space/data_default" # for plots like Fig3B
+
+# parameter space
+D1range <- seq(1,3,0.02) # delta_1, treatment level in E1
+D2range <- seq(1,3,0.02) # delta_2, treatment level in E2
+grid_data <- expand.grid(D1range, D2range)
+# brange <- seq(0.05,3,0.05)
+# drange <- seq(0,3,0.05)
+# rel_cost <- 0.5 # cost of resistance relative to the birth rate (b)
+# grid_data <- expand.grid(brange, drange) %>% filter(Var2<=Var1*(1-rel_cost))
+
+# step and max for range of N(tau) values
+step <- 4000
+nmax <- 100000
+# step <- 10000
+# nmax <- 500000
+
+#################################################################
+
+# default parameters
+ord <- 100 # order of initial population relative to 10^4
+nr2 <- 1*ord # initial R2 population
+nr1 <- 1*ord # initial R1 population
+nr12 <- 0 # initial R12 population
+ns <- 10000*ord # initial S population
+N0 <- ns+nr1+nr2+nr12 # total initial population
+K <- N0 # carrying capacity
+p2 <- 0.25 # probability of mutating from S -> R2
+p1 <- 0.25 # probability of mutating from S -> R1
+mu <- 0.00001 # total mutation rate in E1
+mu2 <- 0.00001 # total mutation rate in E2
+a1 <- mu*p1
+a2 <- mu*p2
+a1_2 <- mu2*p1
+a2_2 <- mu2*p2
+D1 <- 2 # treatment level in E1
+D2 <- 2 # treatment level in E2
+b <- 1 # intrinsic birth rate (of S cells)
+d <- 0.1 # intrinsic death rate
+s <- b-d # growth rate of S cells
+cost <- 0.5 # cost of resistance
+rel_cost <- cost/b
+r <- s-cost # growth rate of resistant cells (R1,R2,R12)
+pi2 <- 1-exp(-2*(r)/(b + d - cost)) # establishment probability
+
+# save parameters
+params_filename <- "params.csv"
+var_names <- ls()
+variables_to_save <- c("b","d","cost","rel_cost","K","D1","D2","nr1","nr2","nr12","ns","N0","a1","a2","a1_2","a2_2","ord")  # add variables to save
+variables_to_save <- intersect(variables_to_save, var_names)
+var_data <- data.frame(t(sapply(variables_to_save, get)))
+write.csv(var_data, file = file.path(main_dir, params_filename), row.names = FALSE) 
+
+
+# define growth functions
+grow_E1 <- function(t, init_state, growth_params){
+  with(as.list(c(init_state, growth_params)),{
+    dS = S*(gs*(1-(S+R1+R2+R12)/K) - D) - S*(a1+a2)
+    dR1 = R1*gr*(1-(S+R1+R2+R12)/K) - R1*a2 + S*a1
+    dR2 = R2*(gr*(1-(S+R1+R2+R12)/K) - D) - R2*a1 + S*a2
+    dR12 = R12*gr*(1-(S+R1+R2+R12)/K) + R2*a1 + R1*a2
+    
+    list(c(dS,dR1,dR2,dR12))
+  }) # end with(as.list...
+}
+
+grow_E2 <- function(t, init_state, growth_params){
+  with(as.list(c(init_state, growth_params)),{
+    dS = S*(gs*(1-(S+R1)/K) - D) - S*a1
+    dR1 = R1*(gr*(1-(S+R1)/K) - D) + S*a1
+    
+    list(c(dS,dR1))
+  }) # end with(as.list...
+}
+
+init_state_E1 <- c(S=ns, R1=nr1, R2=nr2, R12=nr12)
+times_E1 <- seq(0,500,by=0.01)
+ 
+pb = txtProgressBar(min = 0, 
+                    max = nrow(grid_data), 
+                    initial = 0, style=3) 
+stepi <- 0
+for(j in seq(nrow(grid_data))){
+  stepi <- stepi+1
+  setTxtProgressBar(pb,stepi)
+  
+  ######### edit this block for b-d space plots #############
+  D1 <- grid_data[j,1]
+  D2 <- grid_data[j,2]
+  # b <- grid_data[j,1]
+  # d <- grid_data[j,2]
+  # cost <- b*rel_cost
+  # s <- b-d
+  # r <- s-cost
+  # pi2 <- 1-exp(-2*(r)/(b + d - cost)) # establishment probability
+  
+  data_filename <- paste("D1",D1,"_D2",D2,".csv", sep = "")
+  # data_filename <- paste("b",b,"_d",d,".csv", sep = "")
+  ########################################################### 
+  
+  growth_params <- c(K=K, D=D1, gs=s, gr=r, a1=a1, a2=a2)
+  out_E1 <- ode(y=init_state_E1, times=times_E1, func=grow_E1, parms=growth_params)
+  time <- out_E1[,"time"]
+  S <- out_E1[,"S"]
+  R1 <- out_E1[,"R1"]
+  R2 <- out_E1[,"R2"]
+  R12 <- out_E1[,"R12"]
+  T_E1 <- S+R1+R2+R12
+  
+  nmin_idx <- which.min(T_E1)
+  T_E1_before <- T_E1[1:nmin_idx]
+  T_E1_after <- T_E1[nmin_idx:length(T_E1)]
+  nmin_num <- max(0,min(T_E1))
+  ntrange <- seq(nmin_num,nmax,step)
+  
+  PEs_before <- c()
+  PEs_after <- c()
+  for(N_tau in ntrange){
+    idx_before <- which.min(abs(T_E1_before - N_tau))
+    idx_after <- which.min(abs(T_E1_after - N_tau))
+    
+    ## before nmin
+    # growth in E2
+    growth_params_E2 <- c(K=K, D=D2, gs=s, gr=r, a1=a1_2, a2=a2_2)
+    init_state_E2_before <- c(S=S[idx_before], R1=R1[idx_before])
+    times_E2 <- seq(0,200,by=0.1)
+    
+    out_E2_before <- ode(y=init_state_E2_before, times=times_E2, func=grow_E2, parms=growth_params_E2)
+    time_E2_before <- out_E2_before[,"time"]
+    S_E2_before <- out_E2_before[,"S"]
+    R1_E2_before <- out_E2_before[,"R1"]
+    
+    # calculating the extinction probabilities
+    I_S_before <- 0.1*sum(S_E2_before[which(S_E2_before>=1)])
+    I_R1_before <- 0.1*sum(R1_E2_before[which(R1_E2_before>=1)])
+    R12_estimate_before <- 0.01*(a2*sum(R1[1:idx_before])+a1*sum(R2[1:idx_before]))
+    logPE <- -pi2*R2[idx_before] - pi2*a2*I_S_before - pi2*a2*I_R1_before - pi2*R12_estimate_before
+    PE <- exp(logPE)
+    PEs_before <- c(PEs_before, PE)
+    
+    ## after nmin
+    tau_idx <- length(T_E1_before)+idx_after-1
+    # growth in E2
+    init_state_E2_after <- c(S=S[tau_idx], R1=R1[tau_idx])
+    times_E2 <- seq(0,200,by=0.1)
+    
+    out_E2_after <- ode(y=init_state_E2_after, times=times_E2, func=grow_E2, parms=growth_params_E2)
+    time_E2_after <- out_E2_after[,"time"]
+    S_E2_after <- out_E2_after[,"S"]
+    R1_E2_after <- out_E2_after[,"R1"]
+    
+    # calculating the extinction probabilities
+    I_S_after <- 0.1*sum(S_E2_after[which(S_E2_after>=1)])
+    I_R1_after <- 0.1*sum(R1_E2_after[which(R1_E2_after>=1)])
+    R12_estimate_after <- 0.01*(a2*sum(R1[1:tau_idx])+a1*sum(R2[1:tau_idx]))
+    logPE <- -pi2*R2[tau_idx] - pi2*a2*I_S_after - pi2*a2*I_R1_after - pi2*R12_estimate_after
+    PE <- exp(logPE)
+    PEs_after <- c(PEs_after, PE)
+  }
+  
+  PEs_before[which(PE>1)]=1
+  PEs_after[which(PE>1)]=1
+  PE_data <- data.frame(ntrange, PEs_before, PEs_after)
+  colnames(PE_data) <- c("N_tau", "PE_before", "PE_after")
+  
+  write.csv(PE_data, file=file.path(main_dir,data_filename), row.names = FALSE)
+}
+close(pb)
---
+++ plotting/high_prob_regions.R
@@ -0,0 +1,132 @@
+suppressPackageStartupMessages({
+  library(ggplot2)
+  library(dplyr)
+  library(reshape2)
+  library(tidyverse)
+  library(latex2exp)
+  library(viridis)
+  library(metR)
+})
+# Extracts data and created heatmaps showing 
+# high extinction probability regions in the
+# delta_1-delta_2 space (uncommented) and the
+# b-d space (commented)
+
+
+# setting theme for plots
+theme_set(theme_linedraw())
+
+# edit this block 
+#################################################################
+
+main_dir <- "optimal_doses/data_default/"
+# main_dir <- "b-d_space/data_default"
+plots_dir <- "optimal_doses/plots"
+# plots_dir <- "b-d_space/plots"
+
+output_file <- "heatmap_data_default.csv"
+plot_file <- "heatmap_default_before.pdf"
+
+D1range <- seq(1,3,0.02)
+D2range <- seq(1,3,0.02)
+grid_data <- expand.grid(D1range, D2range)
+# brange <- seq(0.05,3,0.05)
+# drange <- seq(0,3,0.05)
+# rel_cost <- read.csv(file=file.path(main_dir,"params.csv"))$rel_cost[1]
+# grid_data <- expand.grid(brange, drange) %>% filter(Var2<=Var1*(1-rel_cost))
+
+hp <- 0.8 # high probability threshold
+
+#################################################################
+
+before <- c() 
+after <- c()
+optimal_ntaus <- c()
+nmins <- c()
+optimal_pes <- c()
+maxtau_before <- c()
+maxtau_after <- c()
+ba_flags <- c()
+
+# initiate text progress bar
+pb = txtProgressBar(min = 0, max = nrow(grid_data), initial = 0, style=3) 
+stepi <- 0
+for(j in seq(nrow(grid_data))){
+  stepi <- stepi+1
+  setTxtProgressBar(pb,stepi)
+  
+  ######### edit this block for b-d space plots #############
+  D1 <- grid_data[j,1]
+  D2 <- grid_data[j,2]
+  # b <- grid_data[j,1]
+  # d <- grid_data[j,2]
+
+  data_filename <- paste("D1",D1,"_D2",D2,".csv",sep = "")
+  # data_filename <- paste("b",b,"_d",d,".csv", sep = "")
+  ###########################################################  
+  
+  PE_data <- read.csv(file=file.path(main_dir,data_filename))
+  
+  # high extinction probability range before N(min)
+  highs_before <- PE_data[which(PE_data$PE_before>hp),1]
+  min_value_before <- ifelse(length(highs_before) == 0, 0, min(highs_before))
+  max_value_before <- ifelse(length(highs_before) == 0, 0, max(highs_before))
+  high_range_before <- max_value_before - min_value_before
+  before <- c(before, high_range_before)
+  maxtau_before <- c(maxtau_before,max_value_before)
+  
+  # high extinction probability range after N(min)
+  highs_after <- PE_data[which(PE_data$PE_after>hp),1]
+  min_value_after <- ifelse(length(highs_after) == 0, 0, min(highs_after))
+  max_value_after <- ifelse(length(highs_after) == 0, 0, max(highs_after))
+  high_range_after <- max_value_after - min_value_after
+  after <- c(after, high_range_after)
+  maxtau_after <- c(maxtau_after, max_value_after)
+  
+  # max extinction probability; ba_flag tells whether it lies
+  # before or after the nadir (0:at nadir, 1: after, -1:before)
+  max_PE <- max(c(PE_data$PE_before, PE_data$PE_after))
+  max_row <- which(PE_data==max_PE, arr.ind = TRUE)[1,1]
+  max_col <- which(PE_data==max_PE, arr.ind = TRUE)[1,2]
+  optimal_pes <- c(optimal_pes, max_PE)
+  ba_flag <- ifelse(max_row==1,0, ifelse(max_col==2, -1, 1))
+  ba_flags <- c(ba_flags, ba_flag)
+  
+  # optimal switching point and N(min)
+  optimal_ntaus <- c(optimal_ntaus, PE_data$n_tau[max_row])
+  nmins <- c(nmins,PE_data[1,1])
+}
+close(pb)
+
+data <- cbind(grid_data, before, after, optimal_ntaus, nmins, ba_flags, optimal_pes, maxtau_before, maxtau_after)
+colnames(data)<- c("X","Y","PE_before","PE_after", "optimal_N_tau","N_min","ba_flag","PE_optimal","max_N_tau_before","max_N_tau_after")
+write.csv(data, file = file.path(plots_dir,output_file), row.names = FALSE)
+
+
+# plotting high extinction probability region heatmap
+data <- read.csv(file=file.path(plots_dir,output_file))
+my_palette <- viridis_pal(option = "rocket")(12)
+hpr_plot <- ggplot(data=data, aes(x=X,y=Y))+
+  # edit this line to plot other quantities
+  geom_contour_filled(aes(z=PE_before),breaks=c(0,3000,7000,12000,16000))+
+  scale_fill_manual(values = my_palette[c(1,3,5,7,10,12)])+
+  geom_contour(aes(z=PE_optimal), col="white", breaks = c(0.9,0.94,0.96,0.98,0.99))+
+  geom_contour(aes(z=optimal_N_tau), col="yellow", breaks = c(1000,5000,10000,30000,50000))+
+  #### edit the next four lines for b-d space plots
+  labs(x=TeX("$delta_{1}$"), y=TeX("$delta_{2}$"))+
+  # geom_line(aes(x=X, y=X*(1-rel_cost)))+ # where d=b-cost
+  # geom_line(aes(x=X, y=X-0.9), linetype=2)+ # where b-d=0.9
+  # ylim(c(0,1.5))+    
+  ####
+  guides(fill = guide_legend(reverse = TRUE))+
+  theme(legend.title=element_blank(), 
+        aspect.ratio = 1,
+        legend.text = element_blank(),
+        legend.position = "none",
+        legend.key.size = unit(0.8,"cm"),
+        text = element_text(size=rel(4.4)))
+
+pdf(file=file.path(plots_dir,plot_file), height=3, width = 3)
+print(hpr_plot)
+dev.off()
+
